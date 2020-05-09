a662e3758d656809b4bf390ca6a2f5e96df7a8e8
Arda Terekeci <x_anthos@hotmail.com>
2020-05-09 15:08:26 +0300

    yorum değişikliği 2

--- BelsisWorkshop.Api/Startup.cs
+++ BelsisWorkshop.Api/Startup.cs
@@ -13,7 +13,7 @@ namespace BelsisWorkshop.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container. asd asd asd
+        // This method gets called by the runtime. Use this method to add services to the container. asd asd asd 32432 423 424
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
